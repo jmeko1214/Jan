3a18dc50c1e2277b13f1bbde5c83050a643feca6
Jan Merwinski <jmerwins@panamint.kcir.pwr.edu.pl>
2020-04-06 09:42:55 +0200

    programowanie obiektowe

---
+++ program_obiekt/z2/src/WyrazenieZesp.cpp
@@ -0,0 +1,142 @@
+#include "WyrazenieZesp.hh"
+#include "LZespolona.hh"
+#include<iostream>
+
+using namespace std;
+
+/*
+ * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
+ * w pliku naglowkowym.
+ */
+
+std::ostream & operator<< (std::ostream &strm, const WyrazenieZesp &Z1)
+{
+  strm<<Z1.Arg1;
+  if(strm.fail())
+    {
+      return strm;
+    }
+  if(Z1.Op==Operator::Op_Dodaj)
+    {
+      strm<<"+";
+    }
+  else if(Z1.Op==Operator::Op_Odejmij)
+    {
+      strm<<"-";
+    }
+  else if(Z1.Op==Operator::Op_Mnoz)
+    {
+      strm<<"*";
+    }
+  else if(Z1.Op==Operator::Op_Dziel)
+    {
+      strm<<"/";
+    }
+  if(strm.fail())
+    {
+      return strm;
+    }
+  strm<<Z1.Arg2;
+  if(strm.fail())
+    {
+      return strm;
+    }
+  strm<<endl;
+}
+
+std::istream &operator>> (std::istream &strm, WyrazenieZesp &WZ)
+{
+  strm>>WZ.Arg1;
+  if(strm.fail())
+    {
+      return strm;
+    }
+  strm >> WZ.Op;
+  if(strm.fail())
+    {
+      return strm;
+    }
+  strm>>WZ.Arg2;
+  if(strm.fail())
+    {
+      return strm;
+    }
+}
+
+
+std::istream &operator>> (std::istream &strm, Operator &Op)
+{
+  char znak;
+  strm >> znak;
+  switch(znak)
+    {
+    case '+':
+      {
+	Op = Op_Dodaj;
+	break;
+      }
+    case '-':
+      {
+	Op = Op_Odejmij;
+	break;
+      }
+    case '*':
+      {
+	Op = Op_Mnoz;
+	break;
+      }
+    case '/':
+      {
+	Op = Op_Dziel;
+	break;
+      }
+    default:
+      {
+	strm.setstate(std::ios::failbit);
+	break;
+      }
+    }
+  return strm;
+}
+
+			    
+LZespolona Oblicz(WyrazenieZesp  WyrZ)
+{
+  LZespolona Wynik;
+  switch(WyrZ.Op)
+    {
+    case Operator::Op_Dodaj:
+      {
+	Wynik=WyrZ.Arg1+WyrZ.Arg2;
+	return Wynik;
+	break;
+      }
+    case Operator::Op_Odejmij:
+      {
+	Wynik=WyrZ.Arg1-WyrZ.Arg2;
+	return Wynik;
+	break;
+      }
+    case Operator::Op_Mnoz:
+      {
+	Wynik=WyrZ.Arg1*WyrZ.Arg2;
+	return Wynik;
+	break;
+      }
+    case Operator::Op_Dziel:
+      {
+	Wynik=WyrZ.Arg1/WyrZ.Arg2;
+	return Wynik;
+	break;
+      }
+    default:
+      {
+	cout<<"Nie rozpoznalem dzialania"<<endl;
+	Wynik.re=0;
+	Wynik.im=0;
+	return Wynik;
+	break;
+      }
+    }
+}
+
---
+++ program_obiekt/z2/src/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include "BazaTestu.hh"
+#include "WyrazenieZesp.hh"
+#include "LZespolona.hh"
+#include "Statystyki.hh"
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    cout << endl;
+    cout << " Brak opcji okreslajacej rodzaj testu." << endl;
+    cout << " Dopuszczalne nazwy to:  latwy, trudny." << endl;
+    cout << endl;
+    return 1;
+  }
+
+
+  BazaTestu   BazaT = { nullptr, 0, 0 };
+
+  if (InicjalizujTest(&BazaT,argv[1]) == false)
+  {
+    cerr << " Inicjalizacja testu nie powiodla sie." << endl;
+    return 1;
+  }
+
+
+  
+  cout << endl;
+  cout << " Start testu arytmetyki zespolonej: " << argv[1] << endl;
+  cout << endl;
+
+  WyrazenieZesp   WyrZ_PytanieTestowe;
+  LZespolona Wynik_Odpowiedz;
+  LZespolona Wynik_Dobry;
+  stat test;
+  float dobra;
+  double percent;
+  cout<< " Witam w moim sprawdzianie z liczb zespolonych" <<endl;
+
+  statystyki(test);
+  
+  while (PobierzNastpnePytanie(&BazaT,&WyrZ_PytanieTestowe))
+  {
+    cout << " To jest twoje zadanie: ";
+    cout << WyrZ_PytanieTestowe;
+    Wynik_Dobry=Oblicz(WyrZ_PytanieTestowe);
+    cout << "Twoja odpowiedz: " << endl;
+    for(int i=0;i<3;i++)
+      {
+	cin >> Wynik_Odpowiedz;
+	if(cin.fail())
+	  {
+	    cout << "Nie uzyskalem odpowiedzi. Podaj jeszcze raz!" << endl;
+	    cin.clear();
+	    cin.ignore(1000, '\n');
+	  }
+	else
+	  {
+	    break;
+	  }
+	if(Wynik_Odpowiedz==Wynik_Dobry)
+	  {
+	    doda_dobra(test);
+	  }
+	else if(Wynik_Odpowiedz!=Wynik_Dobry)
+	  {
+	    doda_zla(test);
+	  }
+      }
+  }
+
+  dobra=liczba_dobrych(test);
+  percent=percent_dobrych(test, dobra);
+  wyswietl_wyniki(dobra, percent);
+  
+  cout << endl;
+  cout << " Koniec testu" << endl;
+  cout << endl;
+
+}
